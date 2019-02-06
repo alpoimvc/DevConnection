8074b12dfecd084f03469d725893a9240f741114
João Alpoim <joao.chaves.g@hotmail.com>
2019-02-06 22:53:28 +0000

    finished api 1.0

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Shema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Create Schema
 const PostSchema = new Schema({
@@ -10,5 +10,47 @@ const PostSchema = new Schema({
   text: {
     type: String,
     required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);
